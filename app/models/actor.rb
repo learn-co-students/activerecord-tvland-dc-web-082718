934800d3f8fe030e145803cd65fff977bd1efc1e
kruchkamgar <kruchkamgar@gmail.com>
2018-09-10 14:30:59 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+      # binding.pry
+      Actor.find_by(first_name: self.first_name)
+      [self.first_name, self.last_name].join(' ')
+    end
+
+    def list_roles
+      # binding.pry
+      character = self.characters
+      character.map { |character|
+        "#{character.name} - #{Show.find(character.show_id).name}"
+      }
+    end
+
 end
